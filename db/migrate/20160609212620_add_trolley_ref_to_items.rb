2fdfae70af9b147a02e47090ed0ae77c780d1cf8
Marco Bagnasco <admin@hecticmojo.com>
2016-06-10 19:29:03 +0100

    Amended conflicts after merge

--- db/migrate/20160609212620_add_trolley_ref_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddTrolleyRefToItems < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :items, :trolley, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160610150153) do
 
   create_table "blue_print_items", force: :cascade do |t|
@@ -37,7 +36,6 @@ ActiveRecord::Schema.define(version: 20160610150153) do
     t.integer  "trolley_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "trolley_id"
   end
 
   add_index "items", ["trolley_id"], name: "index_items_on_trolley_id"
